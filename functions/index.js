6c2c828862acd3e1da01b0b11db32022787f9193
Jan Syring-Lingenfelder <github@jan-sl.de>
2017-11-23 14:02:44 +0100

    TASK: Prepare for standalone hosting
    Use express to build the http endpoint. As Firebase functions don't
    support outgoing network traffic (at least not for normal people) the
    webhook needs to be hosted somewhere else

--- functions/index.js
+++ functions/index.js
@@ -2,17 +2,22 @@
 
 process.env.DEBUG = 'actions-on-google:*';
 const App = require('actions-on-google').DialogflowApp;
-const functions = require('firebase-functions');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-exports.dhbwrapla = functions.https.onRequest((request, response) => {
+let app = express();
+app.set('port', (process.env.PORT || 8080));
+app.use(bodyParser.json({type: 'application/json'}));
+
+app.post('/', function(request, response) {  
   const app = new App({request, response});
-  console.log('Request headers: ' + JSON.stringify(request.headers));
-  console.log('Request body: ' + JSON.stringify(request.body));
-  
-  const  getLectures = require('./lectures').getLectures
-  
+  const getLectures = require('./lectures').getLectures;
   let actionMap = new Map();
   actionMap.set('request.lectures', getLectures);
-  
   app.handleRequest(actionMap);
+});
+
+var server = app.listen(app.get('port'), function() {
+  console.log('App listening on port %s', server.address().port);
+  console.log('Press Ctrl+C to quit');
 });
