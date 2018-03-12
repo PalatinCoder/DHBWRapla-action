86b75f72b47e6e1c8653923cbe6dd04bd0eb6563
Jan Syring-Lingenfelder <github@jan-sl.de>
2018-03-12 09:34:52 +0100

    TASK: Ask for futher action after Rapla URL actions

--- webhook/rapla-url.js
+++ webhook/rapla-url.js
@@ -10,14 +10,14 @@ function setRaplaUrl(app) {
   
   let url = `https://rapla.dhbw-karlsruhe.de/rapla?page=iCal&user=${user}&file=${file}`;
   app.userStorage.raplaurl = url;
-  app.tell(`Okay, ich habe deine Rapla URL ${url} gespeichert.`);
+  app.tell(`Okay, ich habe deine Rapla URL ${url} gespeichert. Kann ich sonst noch etwas für dich tun?`);
 }
 
 function tellRaplaUrl(app) {
-  app.tell(`Ich habe folgende Rapla URL gespeichert: ${app.userStorage.raplaurl}`);
+  app.tell(`Ich habe folgende Rapla URL gespeichert: ${app.userStorage.raplaurl}. Kann ich sonst noch etwas für dich tun?`);
 }
 
 function deleteRaplaUrl(app) {
   app.userStorage.raplaurl = "";
-  app.tell('Okay, ich habe deine Rapla URL gelöscht');
+  app.tell('Okay, ich habe deine Rapla URL gelöscht. Kann ich sonst noch etwas für dich tun?');
 }
